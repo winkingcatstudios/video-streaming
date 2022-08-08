379938df0280c9478a7c000d2c72b95aebb4e08d
winkingcat <dankercher@gmail.com>
2022-08-08 15:14:06 -0400

    navbar skeleton done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,28 +1,49 @@
-import { Notifications, Search } from '@material-ui/icons'
-import React from 'react'
+import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
+import React, { useState } from "react";
 
-import "./navBar.scss"
+import "./navBar.scss";
 
 const Navbar = () => {
-  return (
-    <div className='navbar'>
-        <div className="container">
-            <div className="left">
-                <img src="https://winkingcatstudio.com/winkingcatlogo-transparent-white.png" alt="" />
-                <span>Homepage</span>
-                <span>Series</span>
-                <span>Oneshots</span>
-                <span>New and Popular</span>
-                <span>My List</span>
-            </div>
-            <div className="right">
-                <Search/>
-                <span>KID</span>
-                <Notifications/>
-            </div>
-        </div>
-    </div>
-  )
-}
+    const [isScrolled, setIsScrolled] = useState(false);
 
-export default Navbar
+    window.onscroll = () => {
+      setIsScrolled(window.pageYOffset === 0 ? false : true);
+      return () => (window.onscroll = null);
+    };
+
+  return (
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
+      <div className="container">
+        <div className="left">
+          <img
+            src="https://winkingcatstudio.com/winkingcatlogo-transparent-white.png"
+            alt=""
+          />
+          <span>Homepage</span>
+          <span>Series</span>
+          <span>Oneshots</span>
+          <span>New and Popular</span>
+          <span>My List</span>
+        </div>
+        <div className="right">
+          <Search className="icon" />
+          <span>KID</span>
+          <Notifications className="icon" />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/en/6/62/Kermit_the_Frog.jpg"
+            alt=""
+          />
+          <div className="profile">
+            <ArrowDropDown className="icon" />
+            <div className="options">
+              <span>Settings</span>
+              <span>Logout</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -5,9 +5,173 @@ import Navbar from "../components/navbar/Navbar";
 import "./home.scss";
 
 const Home = () => {
-  return <div className="home">
-    <Navbar/>
-  </div>;
+  return (
+    <div className="home">
+      <Navbar />
+      <img
+        width="100%"
+        src="https://usercontent.one/wp/kirileonard.com/wp-content/uploads/2020/07/LTL_the_drunken_whale_opt.jpg"
+        alt=""
+      />
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+      sodales eget dui dapibus placerat. Duis vel nunc sit amet erat dictum
+      pulvinar non ac odio. Aenean vel lacinia dui. Fusce vel libero gravida,
+      posuere nunc et, ullamcorper leo. Vivamus sed eros massa. Ut in tincidunt
+      felis, in fringilla augue. Etiam hendrerit nunc quis libero dignissim
+      efficitur. In nunc metus, eleifend ut pretium vitae, feugiat vitae nisi.
+      Sed sagittis vestibulum turpis et varius. Fusce tincidunt augue vel erat
+      egestas laoreet. Pellentesque habitant morbi tristique senectus et netus
+      et malesuada fames ac turpis egestas. Ut a nunc a massa laoreet
+      condimentum. Sed imperdiet ligula vel porttitor ullamcorper. Class aptent
+      taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+      himenaeos. Nunc sagittis quis ligula non mattis. Aenean cursus sit amet
+      eros laoreet consectetur. Integer laoreet ut nulla in mollis. Sed vitae
+      fermentum libero. Proin quis rhoncus eros. Nam finibus dolor tortor, vel
+      tristique quam dictum id. Phasellus finibus, magna quis faucibus suscipit,
+      enim metus consectetur turpis, ac consequat augue sem non dui. In vel
+      lectus elementum, egestas neque vel, eleifend lacus. Nulla rutrum at velit
+      vitae dignissim. In vestibulum velit ut semper blandit. Proin suscipit
+      velit nibh, id ornare enim efficitur eu. Integer nec consectetur risus.
+      Aliquam sit amet vestibulum nunc. Maecenas faucibus, quam vel laoreet
+      ultrices, ex erat malesuada massa, a pharetra tellus nulla id nisl.
+      Praesent finibus suscipit ante, eu dictum dui condimentum eget. Praesent
+      accumsan nisi quis tristique vulputate. Integer eget velit leo. Aliquam
+      nisi enim, eleifend a nisi sit amet, condimentum porta nunc. Etiam lorem
+      leo, aliquet nec est at, ornare maximus nisl. Donec eu gravida leo. Sed
+      facilisis nibh neque, ullamcorper dapibus orci dapibus at. Nunc a nulla
+      augue. Mauris in lectus risus. Mauris ante ex, consectetur aliquam arcu
+      ut, ornare eleifend tortor. Nunc ullamcorper justo id mattis dapibus.
+      Pellentesque habitant morbi tristique senectus et netus et malesuada fames
+      ac turpis egestas. Vivamus et velit vitae enim suscipit tristique.
+      Praesent tortor diam, maximus vel rutrum vel, interdum quis ipsum. Sed eu
+      nunc cursus, gravida nisl vestibulum, vulputate lorem. Fusce in neque
+      velit. Sed nibh turpis, semper id lorem ut, vestibulum varius turpis.
+      Etiam fringilla varius mi, vel dignissim nisl volutpat maximus. Sed eros
+      erat, vehicula at lorem vel, porttitor euismod mauris. Cras luctus rhoncus
+      congue. Fusce luctus velit non urna facilisis, vel consequat massa
+      laoreet. Praesent eu nisl bibendum, pharetra risus id, rutrum enim.
+      Vivamus orci nisi, venenatis eu lacinia ut, fermentum ac augue. Praesent
+      sapien sem, fermentum in hendrerit at, pulvinar in tellus. Proin ac
+      ullamcorper ligula. Lorem ipsum dolor sit amet, consectetur adipiscing
+      elit. Pellentesque sodales eget dui dapibus placerat. Duis vel nunc sit
+      amet erat dictum pulvinar non ac odio. Aenean vel lacinia dui. Fusce vel
+      libero gravida, posuere nunc et, ullamcorper leo. Vivamus sed eros massa.
+      Ut in tincidunt felis, in fringilla augue. Etiam hendrerit nunc quis
+      libero dignissim efficitur. In nunc metus, eleifend ut pretium vitae,
+      feugiat vitae nisi. Sed sagittis vestibulum turpis et varius. Fusce
+      tincidunt augue vel erat egestas laoreet. Pellentesque habitant morbi
+      tristique senectus et netus et malesuada fames ac turpis egestas. Ut a
+      nunc a massa laoreet condimentum. Sed imperdiet ligula vel porttitor
+      ullamcorper. Class aptent taciti sociosqu ad litora torquent per conubia
+      nostra, per inceptos himenaeos. Nunc sagittis quis ligula non mattis.
+      Aenean cursus sit amet eros laoreet consectetur. Integer laoreet ut nulla
+      in mollis. Sed vitae fermentum libero. Proin quis rhoncus eros. Nam
+      finibus dolor tortor, vel tristique quam dictum id. Phasellus finibus,
+      magna quis faucibus suscipit, enim metus consectetur turpis, ac consequat
+      augue sem non dui. In vel lectus elementum, egestas neque vel, eleifend
+      lacus. Nulla rutrum at velit vitae dignissim. In vestibulum velit ut
+      semper blandit. Proin suscipit velit nibh, id ornare enim efficitur eu.
+      Integer nec consectetur risus. Aliquam sit amet vestibulum nunc. Maecenas
+      faucibus, quam vel laoreet ultrices, ex erat malesuada massa, a pharetra
+      tellus nulla id nisl. Praesent finibus suscipit ante, eu dictum dui
+      condimentum eget. Praesent accumsan nisi quis tristique vulputate. Integer
+      eget velit leo. Aliquam nisi enim, eleifend a nisi sit amet, condimentum
+      porta nunc. Etiam lorem leo, aliquet nec est at, ornare maximus nisl.
+      Donec eu gravida leo. Sed facilisis nibh neque, ullamcorper dapibus orci
+      dapibus at. Nunc a nulla augue. Mauris in lectus risus. Mauris ante ex,
+      consectetur aliquam arcu ut, ornare eleifend tortor. Nunc ullamcorper
+      justo id mattis dapibus. Pellentesque habitant morbi tristique senectus et
+      netus et malesuada fames ac turpis egestas. Vivamus et velit vitae enim
+      suscipit tristique. Praesent tortor diam, maximus vel rutrum vel, interdum
+      quis ipsum. Sed eu nunc cursus, gravida nisl vestibulum, vulputate lorem.
+      Fusce in neque velit. Sed nibh turpis, semper id lorem ut, vestibulum
+      varius turpis. Etiam fringilla varius mi, vel dignissim nisl volutpat
+      maximus. Sed eros erat, vehicula at lorem vel, porttitor euismod mauris.
+      Cras luctus rhoncus congue. Fusce luctus velit non urna facilisis, vel
+      consequat massa laoreet. Praesent eu nisl bibendum, pharetra risus id,
+      rutrum enim. Vivamus orci nisi, venenatis eu lacinia ut, fermentum ac
+      augue. Praesent sapien sem, fermentum in hendrerit at, pulvinar in tellus.
+      Proin ac ullamcorper ligula. Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit. Pellentesque sodales eget dui dapibus placerat. Duis vel
+      nunc sit amet erat dictum pulvinar non ac odio. Aenean vel lacinia dui.
+      Fusce vel libero gravida, posuere nunc et, ullamcorper leo. Vivamus sed
+      eros massa. Ut in tincidunt felis, in fringilla augue. Etiam hendrerit
+      nunc quis libero dignissim efficitur. In nunc metus, eleifend ut pretium
+      vitae, feugiat vitae nisi. Sed sagittis vestibulum turpis et varius. Fusce
+      tincidunt augue vel erat egestas laoreet. Pellentesque habitant morbi
+      tristique senectus et netus et malesuada fames ac turpis egestas. Ut a
+      nunc a massa laoreet condimentum. Sed imperdiet ligula vel porttitor
+      ullamcorper. Class aptent taciti sociosqu ad litora torquent per conubia
+      nostra, per inceptos himenaeos. Nunc sagittis quis ligula non mattis.
+      Aenean cursus sit amet eros laoreet consectetur. Integer laoreet ut nulla
+      in mollis. Sed vitae fermentum libero. Proin quis rhoncus eros. Nam
+      finibus dolor tortor, vel tristique quam dictum id. Phasellus finibus,
+      magna quis faucibus suscipit, enim metus consectetur turpis, ac consequat
+      augue sem non dui. In vel lectus elementum, egestas neque vel, eleifend
+      lacus. Nulla rutrum at velit vitae dignissim. In vestibulum velit ut
+      semper blandit. Proin suscipit velit nibh, id ornare enim efficitur eu.
+      Integer nec consectetur risus. Aliquam sit amet vestibulum nunc. Maecenas
+      faucibus, quam vel laoreet ultrices, ex erat malesuada massa, a pharetra
+      tellus nulla id nisl. Praesent finibus suscipit ante, eu dictum dui
+      condimentum eget. Praesent accumsan nisi quis tristique vulputate. Integer
+      eget velit leo. Aliquam nisi enim, eleifend a nisi sit amet, condimentum
+      porta nunc. Etiam lorem leo, aliquet nec est at, ornare maximus nisl.
+      Donec eu gravida leo. Sed facilisis nibh neque, ullamcorper dapibus orci
+      dapibus at. Nunc a nulla augue. Mauris in lectus risus. Mauris ante ex,
+      consectetur aliquam arcu ut, ornare eleifend tortor. Nunc ullamcorper
+      justo id mattis dapibus. Pellentesque habitant morbi tristique senectus et
+      netus et malesuada fames ac turpis egestas. Vivamus et velit vitae enim
+      suscipit tristique. Praesent tortor diam, maximus vel rutrum vel, interdum
+      quis ipsum. Sed eu nunc cursus, gravida nisl vestibulum, vulputate lorem.
+      Fusce in neque velit. Sed nibh turpis, semper id lorem ut, vestibulum
+      varius turpis. Etiam fringilla varius mi, vel dignissim nisl volutpat
+      maximus. Sed eros erat, vehicula at lorem vel, porttitor euismod mauris.
+      Cras luctus rhoncus congue. Fusce luctus velit non urna facilisis, vel
+      consequat massa laoreet. Praesent eu nisl bibendum, pharetra risus id,
+      rutrum enim. Vivamus orci nisi, venenatis eu lacinia ut, fermentum ac
+      augue. Praesent sapien sem, fermentum in hendrerit at, pulvinar in tellus.
+      Proin ac ullamcorper ligula. Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit. Pellentesque sodales eget dui dapibus placerat. Duis vel
+      nunc sit amet erat dictum pulvinar non ac odio. Aenean vel lacinia dui.
+      Fusce vel libero gravida, posuere nunc et, ullamcorper leo. Vivamus sed
+      eros massa. Ut in tincidunt felis, in fringilla augue. Etiam hendrerit
+      nunc quis libero dignissim efficitur. In nunc metus, eleifend ut pretium
+      vitae, feugiat vitae nisi. Sed sagittis vestibulum turpis et varius. Fusce
+      tincidunt augue vel erat egestas laoreet. Pellentesque habitant morbi
+      tristique senectus et netus et malesuada fames ac turpis egestas. Ut a
+      nunc a massa laoreet condimentum. Sed imperdiet ligula vel porttitor
+      ullamcorper. Class aptent taciti sociosqu ad litora torquent per conubia
+      nostra, per inceptos himenaeos. Nunc sagittis quis ligula non mattis.
+      Aenean cursus sit amet eros laoreet consectetur. Integer laoreet ut nulla
+      in mollis. Sed vitae fermentum libero. Proin quis rhoncus eros. Nam
+      finibus dolor tortor, vel tristique quam dictum id. Phasellus finibus,
+      magna quis faucibus suscipit, enim metus consectetur turpis, ac consequat
+      augue sem non dui. In vel lectus elementum, egestas neque vel, eleifend
+      lacus. Nulla rutrum at velit vitae dignissim. In vestibulum velit ut
+      semper blandit. Proin suscipit velit nibh, id ornare enim efficitur eu.
+      Integer nec consectetur risus. Aliquam sit amet vestibulum nunc. Maecenas
+      faucibus, quam vel laoreet ultrices, ex erat malesuada massa, a pharetra
+      tellus nulla id nisl. Praesent finibus suscipit ante, eu dictum dui
+      condimentum eget. Praesent accumsan nisi quis tristique vulputate. Integer
+      eget velit leo. Aliquam nisi enim, eleifend a nisi sit amet, condimentum
+      porta nunc. Etiam lorem leo, aliquet nec est at, ornare maximus nisl.
+      Donec eu gravida leo. Sed facilisis nibh neque, ullamcorper dapibus orci
+      dapibus at. Nunc a nulla augue. Mauris in lectus risus. Mauris ante ex,
+      consectetur aliquam arcu ut, ornare eleifend tortor. Nunc ullamcorper
+      justo id mattis dapibus. Pellentesque habitant morbi tristique senectus et
+      netus et malesuada fames ac turpis egestas. Vivamus et velit vitae enim
+      suscipit tristique. Praesent tortor diam, maximus vel rutrum vel, interdum
+      quis ipsum. Sed eu nunc cursus, gravida nisl vestibulum, vulputate lorem.
+      Fusce in neque velit. Sed nibh turpis, semper id lorem ut, vestibulum
+      varius turpis. Etiam fringilla varius mi, vel dignissim nisl volutpat
+      maximus. Sed eros erat, vehicula at lorem vel, porttitor euismod mauris.
+      Cras luctus rhoncus congue. Fusce luctus velit non urna facilisis, vel
+      consequat massa laoreet. Praesent eu nisl bibendum, pharetra risus id,
+      rutrum enim. Vivamus orci nisi, venenatis eu lacinia ut, fermentum ac
+      augue. Praesent sapien sem, fermentum in hendrerit at, pulvinar in tellus.
+      Proin ac ullamcorper ligula.
+    </div>
+  );
 };
 
 export default Home;
